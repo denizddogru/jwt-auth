1334ee670c44e2b79bca89a8cda5a43085ffc2a6
Deniz Doğru <deniz.dogru@venhancer.com>
2024-08-27 16:38:10 +0300

    Core Layer: Adding entities

---
+++ AuthServer.Core/Entity/UserRefreshToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Entity;
+public class UserRefreshToken
+{
+    public string UserId { get; set; }
+    public string RefreshToken { get; set; }
+    public DateTime ExpirationDate { get; set; }
+}
