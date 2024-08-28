426856b5359181895db9f1dc60ea7bf44f74ee68
Deniz Doğru <deniz.dogru@venhancer.com>
2024-08-28 11:17:03 +0300

    ErrorDto added

---
+++ SharedLibrary/ErrorDto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary;
+
+// Tüm API'larda hata yakalanan kısım burası olacak
+public class ErrorDto
+{
+    
+    // Private set: Özelliğin sadece sınıfın iççinden ayarlanabilmesini sağlar.
+    public  List<String> Errors { get; private set; }
+
+    public bool IsShow { get; private set; }
+
+
+    public ErrorDto()
+    {
+        Errors  = new List<String>();
+    }
+
+    public ErrorDto(string error, bool isShow)
+    {
+        Errors.Add(error);
+        isShow = true;
+    }
+
+    public ErrorDto(List<String> errors, bool isShow)
+    {
+        Errors = Errors;
+        IsShow = isShow;
+    }
+
+
+
+}
