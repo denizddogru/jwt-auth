a3a22ea920e8eaab5002334db10547541db3de6c
Deniz Doğru <deniz.dogru@venhancer.com>
2024-08-29 13:41:05 +0300

    IAuthenticationService is the part where we interact directly with the API. Get Email and Pass from the user, if it  matches our API provides with AccessToken and RefreshToken

---
+++ AuthServer.Core/Dtos/ClientLoginDto.cs
@@ -0,0 +1,6 @@
+﻿namespace AuthServer.Core.Dtos;
+public class ClientLoginDto
+{
+    public string ClientId { get; set; }
+    public string ClientSecret { get; set; }
+}
---
+++ AuthServer.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,20 @@
+﻿using AuthServer.Core.Dtos;
+using SharedLibrary.Dtos;
+
+namespace AuthServer.Core.Services;
+
+
+// Direkt olarak API'ye göndereceğimiz kısım, o yüzden response bulunacak.
+// Kullanıcıdan EMail, pass alıyoruz ve geriye Token döönüyoruz. 
+
+public interface IAuthenticationService
+{
+
+    Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+
+    Task<Response<TokenDto>> CreateTokenByRefreshTokenAsync(string refreshToken);
+
+    Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken);
+
+    Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLoginDto loginDto);
+}
