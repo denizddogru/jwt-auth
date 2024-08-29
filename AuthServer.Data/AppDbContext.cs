ed92174e44293cafc09bab190728caa6234ca4a4
Deniz Doğru <deniz.dogru@venhancer.com>
2024-08-29 17:11:28 +0300

    Data Layer: AppDbContext class created, this will be the class where we define our DbSets, our tables. Also we configure our DB options

---
+++ AuthServer.Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using AuthServer.Core.Entity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServer.Data;
+
+// Identity üyelik tabloları
+//  Kullanıcı ile ilgili tüm db setler IdentityDbContext'ten gelicek
+public class AppDbContext : IdentityDbContext<AppUser, IdentityRole,string>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
