497ca413bdbfaa3c3510007db5e23ee875ea1e3a
Deniz Doğru <deniz.dogru@venhancer.com>
2024-08-31 15:03:12 +0300

    Data Layer: Last chapter of data layer, here we implement the UnitOfWork pattern which lets us SaveChanges and SaveChangesAsync. The DB will be updated with the save changes command.

---
+++ AuthServer.Data/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using AuthServer.Core.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServer.Data;
+public class UnitOfWork : IUnitOfWork
+{
+
+    private readonly DbContext _context;
+
+    public void Commit()
+    {
+        _context.SaveChanges();   
+    }
+
+    public async Task CommitAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+
+    
+}
