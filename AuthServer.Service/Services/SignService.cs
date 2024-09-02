9c671080929c905f24e37520f8199104ed0b70ee
Deniz Doğru <deniz.dogru@venhancer.com>
2024-09-02 13:53:38 +0300

    Service Layer: SignService: Here we create a symmetricSecurityKey which allows crpytographic operations. This method returns a SecurityKey containing public and private keys

---
+++ AuthServer.Service/Services/SignService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace AuthServer.Service.Services;
+
+// Private key şifreler, public key doğrular
+
+
+// A
+
+// B
+
+
+// B bir public key ve private key oluşturur
+// daha sonra public key'i A'yı gönderir.
+// A, word dosyasıyla gelen key'i imzalar.
+
+// Özetle, B A'ya ulaşırken A'nın public key'ini kullanarak şifrelenmiş mesajlar gönderebilir veya A'nın dijital imasını doğrulayabilir
+// bu şekilde A'nın ( yani bizim API ) private anahtarı hiçbir zaman B'ye açıklanmaz.
+
+
+internal static class SignService
+{
+    public static SecurityKey GetSymmetricSecurityKey(string securityKey)
+    {
+        return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+    }
+}
