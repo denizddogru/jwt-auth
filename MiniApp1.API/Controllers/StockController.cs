80dc562d32edf21ca15fe3f2c6ae6a7c76144b5e
Deniz Doğru <deniz.dogru@venhancer.com>
2024-09-06 16:31:13 +0300

    Adding controllers for MiniAPI's

---
+++ MiniApp1.API/Controllers/StockController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace MiniApp1.API.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class StockController : ControllerBase
+{
+    public IActionResult GetStock()
+    {
+
+        // veri tabanında name alanına ait istediğimiz bilgiyi alabiliriz artık.
+
+        var userName = HttpContext.User.Identity.Name;
+
+        // veri tabanında Id alanına ait istediğimiz bilgiyi alabiliriz artık.
+
+        var userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+
+        return Ok($"Stock Operations => Username: {userName} - UserId: {userId}");
+
+    }
+}
---
+++ MiniApp2.API/Controllers/InvoiceController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace MiniApp1.API.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class Invoice : ControllerBase
+{
+    public IActionResult GetInvoices()
+    {
+
+        // veri tabanında name alanına ait istediğimiz bilgiyi alabiliriz artık.
+
+        var userName = HttpContext.User.Identity.Name;
+
+        // veri tabanında Id alanına ait istediğimiz bilgiyi alabiliriz artık.
+
+        var userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+
+        return Ok($"Invoice Operations => Username: {userName} - UserId: {userId}");
+
+    }
+}
